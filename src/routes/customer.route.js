312008a03d450794667de80d48e5a7a2ef5498be
An Hung Nguyen <nguyenanhung1903@gmail.com>
2024-10-07 18:50:54 +0700

    DEK-77: Change route name from 'user' to 'customer'

--- src/routes/customer.route.js
+++ src/routes/customer.route.js
@@ -1,6 +1,6 @@
 import express from "express";
-import categoryRoute from "./user/category.route.js";
-import productRoute from "./user/product.route.js";
+import categoryRoute from "./customer/category.route.js";
+import productRoute from "./customer/product.route.js";
 
 const router = express.Router();
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,12 @@
 import express from "express";
 
 import authRoute from "./auth.route.js";
-import userRoute from "./user.route.js";
+import customerRoute from "./customer.route.js";
 import adminRoute from "./admin.route.js";
 
 const router = express.Router();
 router.use("/auth", authRoute);
-router.use("/user", userRoute);
+router.use("/customer", customerRoute);
 router.use("/admin", adminRoute);
 
 export default router;
