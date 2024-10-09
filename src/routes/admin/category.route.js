95e4a1951706746e7a9ca5ecb267c9443f5f9f85
An Hung Nguyen <nguyenanhung1903@gmail.com>
2024-10-09 11:31:25 +0700

    DEK-83: Create delete category flow and update the API document
    Create delete category function and define the route for this function\nUpdate API document for category

--- src/routes/admin/category.route.js
+++ src/routes/admin/category.route.js
@@ -5,11 +5,18 @@ import categoryValidation from "../../validations/category.validation.js";
 import categoryController from "../../controllers/category.controller.js";
 const router = express.Router();
 
-router.use(passport.authenticate('jwt', { session: false }));
+router.use(passport.authenticate("jwt", { session: false })); // Protect all routes
+
 router.post(
     "/create",
     validate(categoryValidation.createCategory),
     categoryController.createCategory
+  );
+
+router.delete(
+    "/:categoryId",
+    validate(categoryValidation.deleteCategory),
+    categoryController.deleteCategory
   );
 
 export default router;
