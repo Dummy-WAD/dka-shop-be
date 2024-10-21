5bc663d910ff757f3556815e734484920f53f8f8
hongdiem <mingukmanse673@gmail.com>
2024-10-22 00:28:01 +0700

    DEK-123 create api to view product detail for customer

--- src/routes/customer/product.route.js
+++ src/routes/customer/product.route.js
@@ -1,7 +1,6 @@
 
 import express from "express";
 import passport from "passport";
-import { isAdmin, isCustomer } from "../../middlewares/authorization.js";
 import validate from "../../middlewares/validate.js";
 import productController from "../../controllers/product.controller.js";
 import productValidation from "../../validations/product.validation.js";
