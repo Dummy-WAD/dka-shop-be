c07fc684d531aab41e665b3e5a50c47393fe0680
winterrr03 <dong.huynhquang21503@gmail.com>
2024-10-22 20:18:58 +0700

    DEK-124: Create API for customer to view products

--- src/validations/product.validation.js
+++ src/validations/product.validation.js
@@ -23,8 +23,27 @@ const getProductDetail = {
     })
 };
 
+const getProductsForCustomer = {
+    query: Joi.object().keys({
+        name: Joi.string().trim().max(30).optional(),
+        categoryId: Joi.number().integer().positive().optional(),
+        sortBy: Joi.string().valid('price', 'updatedAt').default('updatedAt'),
+        order: Joi.string().valid('asc', 'desc').default('desc'),
+        page: Joi.number().integer().min(1).default(1),
+        limit: Joi.number().integer().min(1).max(100).default(10),
+        priceStart: Joi.number().min(0).optional(),
+        priceEnd: Joi.number().min(0).optional(),
+    }).custom((value, helpers) => {
+        if (value.priceEnd < value.priceStart) {
+            return helpers.error('priceEnd must be greater than or equal to priceStart');
+        }
+        return value;
+    })
+};
+
 export default {
     getProducts,
     deleteProduct,
-    getProductDetail
+    getProductDetail,
+    getProductsForCustomer
 }
