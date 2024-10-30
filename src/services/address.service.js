8ce260a81a7ef9f4c5ee7536a6a002765e1dc386
Quang Nguyen <90626906+lightx26@users.noreply.github.com>
2024-10-30 13:59:17 +0700

    DEK-164: Business logic to update their address: (#68)
    * DEK-164: Business logic to update their address:
    - Get address details
    - Get provinces, districts, wards
    - API to update address information
    - API to set address as default
    
    * DEK-164: Fix validation: provinceId and districtId is string
    
    * DEK-164: Add product update body validation
    
    * DEK-164: Optimize set default address query

--- src/services/address.service.js
+++ src/services/address.service.js
@@ -49,6 +49,60 @@ const getCustomerAddresses = async (customerId) => {
     return addresses;
 }
 
+const getAddressDetails = async (customerId, addressId) => {
+    const address = await db.address.findOne({
+        where: {
+            id: addressId,
+            customerId: customerId
+        },
+        include: [
+            {
+                model: db.ward,
+                as: 'ward',
+                attributes: ['nameEn', 'districtId'],
+                include: [
+                    {
+                        model: db.district,
+                        as: 'district',
+                        attributes: ['nameEn', 'provinceId'],
+                        include: [
+                            {
+                                model: db.province,
+                                as: 'province',
+                                attributes: ['nameEn']
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    });
+
+    if (!address) {
+        throw new ApiError(httpStatus.NOT_FOUND, 'Address not found');
+    }
+
+    const addressDetails = {
+        id: address.id,
+        localAddress: address.localAddress || null,
+        ward: {
+            id: address.wardId,
+            nameEn: address.ward?.nameEn,
+        },
+        district: {
+            id: address.ward?.districtId,
+            nameEn: address.ward?.district?.nameEn,
+        },
+        province: {
+            id: address.ward?.district?.provinceId,
+            nameEn: address.ward?.district?.province?.nameEn,
+        },
+        isDefault: address.isDefault,
+    };
+
+    return addressDetails;
+}
+
 const deleteAddress = async (customerId, addressId) => {
 
     const address = await db.address.findOne({
@@ -73,7 +127,107 @@ const deleteAddress = async (customerId, addressId) => {
     });
 }
 
+const getAllProvinces = async () => {
+    return await db.province.findAll({
+        attributes: ['id', 'nameEn'],
+        order: [['nameEn', 'ASC']]
+    });
+}
+
+const getAllDistrictsInProvince = async (provinceId) => {
+    return await db.district.findAll({
+        where: {
+            provinceId: provinceId
+        },
+        attributes: ['id', 'nameEn'],
+        order: [['nameEn', 'ASC']]
+    });
+}
+
+const getAllWardsInDistrict = async (districtId) => {
+    return await db.ward.findAll({
+        where: {
+            districtId: districtId
+        },
+        attributes: ['id', 'nameEn'],
+        order: [['nameEn', 'ASC']]
+    });
+}
+
+const updateAddressInfo = async (customerId, addressId, addressDetails) => {
+    const address = await db.address.findOne({
+        where: {
+            id: addressId,
+            customerId: customerId
+        }
+    });
+
+    if (!address) {
+        throw new ApiError(httpStatus.NOT_FOUND, 'Address not found');
+    }
+
+    const ward = await db.ward.findOne({
+        where: {
+            id: addressDetails.wardId
+        },
+        attributes: ['id']
+    });
+
+    if (!ward) {
+        throw new ApiError(httpStatus.NOT_FOUND, 'Ward not found');
+    }
+
+    await db.address.update({
+        localAddress: addressDetails.localAddress,
+        wardId: addressDetails.wardId,
+    }, {
+        where: {
+            id: addressId
+        }
+    });
+}
+
+const setAddressAsDefault = async (customerId, addressId) => {
+    const address = await db.address.findOne({
+        where: {
+            id: addressId,
+            customerId: customerId
+        }
+    });
+
+    if (!address) {
+        throw new ApiError(httpStatus.NOT_FOUND, 'Address not found');
+    }
+
+    if (address.isDefault) {
+        throw new ApiError(httpStatus.BAD_REQUEST, 'Address is already default');
+    }
+
+    await db.address.update({
+        isDefault: false
+    }, {
+        where: {
+            customerId: customerId,
+            isDefault: true
+        }
+    });
+
+    await db.address.update({
+        isDefault: true
+    }, {
+        where: {
+            id: addressId
+        }
+    });
+}
+
 export default {
     getCustomerAddresses,
-    deleteAddress
+    getAddressDetails,
+    getAllProvinces,
+    getAllDistrictsInProvince,
+    getAllWardsInDistrict,
+    deleteAddress,
+    updateAddressInfo,
+    setAddressAsDefault
 };
--- src/validations/address.validation.js
+++ src/validations/address.validation.js
@@ -1,11 +1,36 @@
 import Joi from 'joi';
 
-const deleteAddress = {
+const getAddressDetails = {
     params: Joi.object().keys({
         addrId: Joi.number().integer().positive().required()
     })
 };
 
+const getAllDistrictsInProvince = {
+    query: Joi.object().keys({
+        provinceId: Joi.string().trim().required()
+    })
+};
+
+const getAllWardsInDistrict = {
+    query: Joi.object().keys({
+        districtId: Joi.string().trim().required()
+    })
+}
+
+const updateAddressInfo = {
+    params: Joi.object().keys({
+        addrId: Joi.number().integer().positive().required()
+    }),
+    body: Joi.object().keys({
+        wardId: Joi.string().trim().required(),
+        localAddress: Joi.string().trim().required()
+    })
+}
+
 export default {
-    deleteAddress
+    getAddressDetails,
+    getAllDistrictsInProvince,
+    getAllWardsInDistrict,
+    updateAddressInfo
 }
