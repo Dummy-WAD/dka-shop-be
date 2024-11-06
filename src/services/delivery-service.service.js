971900b91ff5f6826e341a32f30bce7e87edbbb6
lightx26 <ncnhatquang26@gmail.com>
2024-11-06 11:03:53 +0700

    DEK-204: Fix field name: is_active to isActive

--- src/services/delivery-service.service.js
+++ src/services/delivery-service.service.js
@@ -3,7 +3,7 @@ import db from "../models/models/index.js";
 const getAllActiveDeliveryServices = async () => {
     return await db.deliveryService.findAll({
         where: {
-            is_active: true
+            isActive: true
         },
         attributes: ['id', 'name', 'description', 'deliveryFee']
     })
