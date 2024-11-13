69eefaad74429cd0c0e5a8c6e460d95b4cf2d00e
winterrr03 <dong.huynhquang21503@gmail.com>
2024-11-13 15:48:28 +0700

    DEK-226: Statistic revenue by time

--- src/validations/statistics.validation.js
+++ src/validations/statistics.validation.js
@@ -1,5 +1,5 @@
 import Joi from 'joi';
-import { OrderStatus, StatisticsPeriod } from '../utils/enums.js';
+import { StatisticsPeriod } from '../utils/enums.js';
 
 const getNewCustomerStatistics = {
     query: Joi.object().keys({
@@ -15,7 +15,15 @@ const getOrderStatistics = {
     })
 };
 
+const getRevenueStatistics = {
+    query: Joi.object().keys({
+        type: Joi.string().valid(...Object.values(StatisticsPeriod)).required(),
+        limit: Joi.number().integer().min(1).max(12).default(4)
+    })
+};
+
 export default {
     getNewCustomerStatistics,
-    getOrderStatistics
+    getOrderStatistics,
+    getRevenueStatistics
 }
